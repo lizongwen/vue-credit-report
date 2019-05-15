fd6cb9a4d2255c2807d97c41dac89b3b3da57da4
lizongwen <93700983@qq.com>
2019-05-15 17:41:51 +0800

    1

--- src/components/SideMenu.vue
+++ src/components/SideMenu.vue
@@ -30,19 +30,6 @@ export default {
   data() {
     return {
       keyArr: ['about'],
-      //   menus: [
-      //     { title: '仪表盘', icon: 'dashboard', key: 'index' },
-      //     { title: '表单页', icon: 'video-camera', key: 'home' },
-      //     {
-      //       title: '表格页',
-      //       icon: 'area-chart',
-      //       children: [
-      //         { title: '地图组件', icon: 'user', key: 'map' },
-      //         { title: '子菜单二', icon: 'setting', key: 'set' }
-      //       ]
-      //     },
-      //     { title: '个人中心', icon: 'lock', key: 'base' }
-      //   ]
     }
   },
   mounted() {
